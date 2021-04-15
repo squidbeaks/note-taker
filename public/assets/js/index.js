b95bc16a1248b922288ff151e0c428e4e0b4d8ab
Leah Russell <leahsigridrussell@gmail.com>
2021-04-14 20:52:34 -0700

    add notes through the FE

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -37,9 +37,20 @@ const saveNote = (note) =>
   fetch('/api/notes', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
     },
-    body: JSON.stringify(note),
+    body: JSON.stringify(note)
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    alert('Error: ' + response.statusText);
+  })
+  .then(postResponse => {
+    console.log(postResponse);
+    alert('Thank you for adding a note!');
   });
 
 const deleteNote = (id) =>
--- server.js
+++ server.js
@@ -80,6 +80,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
 });
