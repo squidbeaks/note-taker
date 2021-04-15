4f13d81b6c77a55defe5657b92a36e38cc2b928c
Leah Russell <leahsigridrussell@gmail.com>
2021-04-14 19:04:12 -0700

    Added get for individual note and post request

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const { notes } = require('./db/db');
+const fs = require('fs');
+const path = require('path');
+const { type } = require('os');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 function filterByQuery(query, notesArray) {
     let filteredResults = notesArray;
 
-    if (query.id) {
-        filteredResults = filteredResults.filter(note => note.id === query.id);
-    }
     if (query.title) {
         filteredResults = filteredResults.filter(note => note.title === query.title);
     }
@@ -19,6 +22,31 @@ function filterByQuery(query, notesArray) {
     return filteredResults;
 }
 
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+}
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
+
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    return true;
+}
+
 app.get('/api/notes', (req, res) => {
     let results = notes;
     if (req.query) {
@@ -26,6 +54,26 @@ app.get('/api/notes', (req, res) => {
     }
     res.json(results);
 });
+
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted!');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
