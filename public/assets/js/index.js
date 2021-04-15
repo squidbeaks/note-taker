5a2152a8f30304b7fcaa8261f55b67e465b58a69
Leah Russell <leahsigridrussell@gmail.com>
2021-04-14 23:13:36 -0700

    refactoring code

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -182,6 +182,7 @@ const renderNoteList = async (notes) => {
 
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
+//const getAndRenderNotes = () => getNotes().then(notes => renderNoteList(notes));
 
 if (window.location.pathname === '/notes') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
--- server.js
+++ server.js
@@ -1,98 +1,20 @@
 const express = require('express');
-const { notes } = require('./db/db');
-const fs = require('fs');
-const path = require('path');
-const { type } = require('os');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-function filterByQuery(query, notesArray) {
-    let filteredResults = notesArray;
-
-    if (query.title) {
-        filteredResults = filteredResults.filter(note => note.title === query.title);
-    }
-    if (query.text) {
-        filteredResults = filteredResults.filter(note => note.text === query.text);
-    }
-    return filteredResults;
-}
-
-function findById(id, notesArray) {
-    const result = notesArray.filter(note => note.id === id)[0];
-    return result;
-}
-
-function createNewNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({ notes: notesArray }, null, 2)
-    );
-    return note;
-}
-
-function validateNote(note) {
-    if (!note.title || typeof note.title !== 'string') {
-        return false;
-    }
-    if (!note.text || typeof note.text !== 'string') {
-        return false;
-    }
-    return true;
-}
-
-app.get('/api/notes', (req, res) => {
-    let results = notes;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-});
-
-app.get('/api/notes/:id', (req, res) => {
-    const result = findById(req.params.id, notes);
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404);
-    }
-});
-
-app.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-
-    if (!validateNote(req.body)) {
-        res.status(400).send('The note is not properly formatted!');
-    } else {
-        const note = createNewNote(req.body, notes);
-        res.json(note);
-    }
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-})
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
 });
-
-
 
 // GIVEN a note-taking application
 // WHEN I click on the link to the notes page
