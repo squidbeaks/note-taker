fcb778b17d9b7cb96a7d7792eee2b7854f50b37f
Leah Russell <leahsigridrussell@gmail.com>
2021-04-14 18:43:45 -0700

    Adding Heroku and filtering to get request

--- server.js
+++ server.js
@@ -7,21 +7,24 @@ const app = express();
 function filterByQuery(query, notesArray) {
     let filteredResults = notesArray;
 
+    if (query.id) {
+        filteredResults = filteredResults.filter(note => note.id === query.id);
+    }
     if (query.title) {
         filteredResults = filteredResults.filter(note => note.title === query.title);
     }
     if (query.text) {
         filteredResults = filteredResults.filter(note => note.text === query.text);
     }
+    return filteredResults;
 }
 
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
-    // let results = notes;
-    // if (req.query) {
-    //     results = filterByQuery(req.query results);
-    // }
-    // res.json(results);
+    let results = notes;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
 });
 
 app.listen(PORT, () => {
